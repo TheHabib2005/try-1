7ac2ef1e6d3eb55ea10d0b7d262a56f0151cd0d6
HABIB <mdwear2005@gmail.com>
2024-08-02 18:44:37 +0600

    d

--- components/userAvater.tsx
+++ components/userAvater.tsx
@@ -6,15 +6,15 @@ import Link from 'next/link';
 import React from 'react'
 
 const UserAvater = async () => {
-    const cookieStore = cookies();
-    const token = cookieStore.get("auth-token")?.value;
-    let { userData } = await verify(token!, process.env.JWT_SECRET!);
-    console.log(userData);
+    // const cookieStore = cookies();
+    // const token = cookieStore.get("auth-token")?.value;
+    // let { userData } = await verify(token!, process.env.JWT_SECRET!);
+    // console.log(userData);
 
 
     return (
         <div className=' bg-gray-100 md:flex hidden items-center justify-center rounded-full dark:bg-zinc-900 dark:text-white'  >
-            <img src={userData.profilePicture} alt="" className='w-10 rounded-full cursor-pointer' />
+            {/* <img src={userData.profilePicture} alt="" className='w-10 rounded-full cursor-pointer' /> */}
 
         </div>
     )
