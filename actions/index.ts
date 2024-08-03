f272d1135144a0e48ebb96ce7186c7b6de9d5521
HABIB <mdwear2005@gmail.com>
2024-08-03 20:14:32 +0600

    g

--- actions/index.ts
+++ actions/index.ts
@@ -2,8 +2,4 @@
 
 import { cookies } from "next/headers";
 
-export const setCookie = () => {
-  let co = cookies();
-  co.set("redirect", "qwerqweqw");
-  console.log("set Cookie");
-};
+export const setCookie = () => {};
--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import { useCartStore } from "@/zustant-store/useCartStore";
+import axios from "axios";
+import React from "react";
 
 const CheckoutForm = () => {
+    const { cart, totalAmount } = useCartStore()
     return (
-        <form className="col-span-4  p-6 max-w-full ">
+        <div className="col-span-4  p-6 max-w-full ">
             <h1 className="text-2xl my-5 text-white font-semibold  text-center">
                 Checkout Form
             </h1>
             <div>
-                <h1 className="text-xl text-white font-semibold font-sans">
-                    Contact
-                </h1>
+                <h1 className="text-xl text-white font-semibold font-sans">Contact</h1>
                 <div className="relative mt-4">
                     <span className="text-md text-zinc-500 mb-2 inline-flex z-10">
                         Email or mobile phone number
@@ -34,7 +35,6 @@ const CheckoutForm = () => {
                     Shipping address
                 </h1>
 
-
                 <div className="relative mt-4">
                     <span className="text-md text-zinc-500 mb-2 inline-flex z-10">
                         Full Address
@@ -42,7 +42,7 @@ const CheckoutForm = () => {
                     <input
                         type="text"
                         className="w-full p-3 bg-transparent outline-0 border border-zinc-800 rounded-md focus:border-blue-800 text-white"
-                        placeholder='area-state-country'
+                        placeholder="area-state-country"
                     />
                 </div>
 
@@ -53,18 +53,57 @@ const CheckoutForm = () => {
 
                     <div className="flex items-center gap-x-2 ">
                         <input type="radio" checked={true} name="" id="cod" />
-                        <label htmlFor="cod" className="text-white font-semibold text-md cursor-pointer"> Cash on delivery</label>
+                        <label
+                            htmlFor="cod"
+                            className="text-white font-semibold text-md cursor-pointer"
+                        >
+                            {" "}
+                            Cash on delivery
+                        </label>
                     </div>
-
-
                 </div>
 
-                <button className="w-full mt-10 p-4 cursor-pointer rounded-md bg-blue-700 hover:bg-blue-900 text-white font-semibold">
+                <button
+                    className="w-full mt-10 p-4 cursor-pointer rounded-md bg-blue-700 hover:bg-blue-900 text-white font-semibold"
+                    onClick={async () => {
+                        let product = {
+                            userId: "66ae33cf86f7df652eec97a3",
+                            productId: "123456r",
+                            product:
+
+                                cart.map((item) => {
+                                    return {
+                                        title: item.title,
+                                        price: item.price,
+                                        thumbnail: item.thumbnail,
+                                        quantity: item.quantity,
+                                    }
+                                })
+
+
+                            ,
+                            totalAmount: totalAmount,
+                            paymentMethod: "cod",
+                            orderStatus: "prossesing",
+                            deliveryAddress: "signboard",
+                        };
+
+
+                        try {
+                            let res = await axios.post("http://localhost:8000/order/create", product)
+                            console.log(res.data);
+                        } catch (error) {
+                            console.log(error);
+
+                        }
+
+                    }}
+                >
                     Place Order
                 </button>
             </div>
-        </form>
-    )
-}
+        </div>
+    );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
