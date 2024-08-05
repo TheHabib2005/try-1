57201afa2cb130969c0df807b5029779c766f052
HABIB <mdwear2005@gmail.com>
2024-08-05 06:25:35 +0600

    s

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function Login() {
     const handleLogin = async () => {
         try {
-            const response = await axios.get('http://localhost:8000/sign-in', { withCredentials: true });
+            const response = await axios.get('http://localhost:8000/set-cookie', { withCredentials: true });
             console.log(response.data);
         } catch (error) {
             console.error('Login error:');
